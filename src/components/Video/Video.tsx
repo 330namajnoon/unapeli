4c16c0017febf3be45d6b1b5272372968908beb5
Sina Majnoon <sina.majnoon@digimobil.es>
2024-09-11 12:56:57 +0200

    fix video controller

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -12,19 +12,37 @@ const Video = ({ socket }: VideoProps) => {
 
   useEffect(() => {
     socket.on("videoOnChange", (comand: string) => {
-        setSocketCalled(true);
-        setTimeout(() => setSocketCalled(false), 1000);
-        eval(comand);
+      setSocketCalled(true);
+      setTimeout(() => setSocketCalled(false), 1000);
+      eval(comand);
     });
   }, []);
   return (
-    <Styles.Video 
-        ref={video}
-        onPlay={() => !socketCalled && socket.emit("videoOnChange", "video.current.play()")} 
-        onPause={() => !socketCalled && socket.emit("videoOnChange", "video.current.pause()")}
-        onEnded={() => !socketCalled && socket.emit("videoOnChange", "video.current.play()")}
-        onSeeked={(e: any) => !socketCalled && socket.emit("videoOnChange", `video.current.currentTime = ${e.target.currentTime}`)}
-        controls>
+    <Styles.Video
+      ref={video}
+      onPlay={(e: any) =>
+        !socketCalled &&
+        socket.emit(
+          "videoOnChange",
+          `video.current.currentTime = ${e.target.currentTime};video.current.play();`
+        )
+      }
+      onPause={(e: any) =>
+        !socketCalled &&
+        socket.emit(
+          "videoOnChange",
+          `video.current.currentTime = ${e.target.currentTime}; video.current.pause();`
+        )
+      }
+      onSeeked={(e: any) =>
+        !socketCalled &&
+        socket.emit(
+          "videoOnChange",
+          `video.current.currentTime = ${e.target.currentTime}`
+        )
+      }
+      controls
+    >
       <source
         src="https://bellachao.zapto.org/downloads/Inside_Out_2_2024_1080p_WEBRip_x264_AAC5_1_video_converter_com.mp4"
         type="video/mp4"
